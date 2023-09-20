437017b260ac60e6d3a31cadfa401b51061804e0
Miguel Sandoval <mrsandoval@mail.csuchico.edu>
2023-09-20 11:37:09 -0700

    added forced constraints resolved #1

--- main.cpp
+++ main.cpp
@@ -4,13 +4,17 @@ const int RGB_HEX_LENGTH = 7;
 
 int main(){
     std::string input;
-
     do{
         std::cout << "Enter a color in hex format (#RRGGBB):";
-        std::getline(cin, input);
+        std::getline(std::cin, input);
 
-        if( input.size() != RGB_HEX_LENGTH ){
-            std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+        if( input.size() != RGB_HEX_LENGTH )
+            std::cout << "Wrong size.\n";
+        if( input[0] != '#')
+            std::cout << "Needs to start with '#'\n";
+        for(int i = 1; i <= RGB_HEX_LENGTH; i++){
+            if(!isdigit(input[i]) && !(input[i] >= 65 && input[i] <= 70) && !(input[i] >= 97 && input[i] <= 102) )
+                std::cout << "Needs to contain digit or letter a-f (upper or lower)";
         }
     }while( input.size() != RGB_HEX_LENGTH );
 
